b9aa7debce9128a2d3b5a3fcbe0d64f91a32a13c
kelly843397 <kelly.soumbo@gmail.com>
2024-10-07 21:58:12 +0200

    test file ProductManager et models/Product.php ok

---
+++ managers/ProductManager.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class ProductManager extends AbstractManager
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    // Méthode pour récupérer tous les produits
+    public function findAll(): array
+    {
+        $query = $this->db->prepare('SELECT * FROM products');
+        $query->execute();
+
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        $products = [];
+
+        foreach ($result as $item) {
+            $product = new Product(
+                $item['id'],
+                $item['name'],
+                $item['picture'],
+                $item['price'],
+                $item['category_id']
+            );
+            $products[] = $product;
+        }
+
+        return $products;
+    }
+}
---
+++ models/Product.php
@@ -0,0 +1,67 @@
+<?php
+
+class Product
+{
+    private ?int $id;
+    private string $name;
+    private string $picture;
+    private float $price;
+    private int $categoryId;
+
+    // Constructeur
+    public function __construct(?int $id = null, string $name, string $picture, float $price, int $categoryId)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->picture = $picture;
+        $this->price = $price;
+        $this->categoryId = $categoryId;
+    }
+
+    // Getters
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPicture(): string
+    {
+        return $this->picture;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function getCategoryId(): int
+    {
+        return $this->categoryId;
+    }
+
+    // Setters
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setPicture(string $picture): void
+    {
+        $this->picture = $picture;
+    }
+
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function setCategoryId(int $categoryId): void
+    {
+        $this->categoryId = $categoryId;
+    }
+}
