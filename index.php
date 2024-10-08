19b975b5a2b2d9f0bfbc4ea0531eae6b57532adb
kelly843397 <kelly.soumbo@gmail.com>
2024-10-08 20:34:29 +0200

    create file AbstractController.php

--- index.php
+++ index.php
@@ -6,3 +6,10 @@ require "vendor/autoload.php";
 // charge le contenu du .env dans $_ENV
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+require_once 'controllers/AbstractController.php';
+require_once 'controllers/TestController.php'; // Chemin vers TestController
+
+// Instancier le contrôleur et tester le rendu du template
+$controller = new TestController();
+$controller->testRender(); // Test du rendu Twig
