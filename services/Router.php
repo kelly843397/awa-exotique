fb5001afff203ea180e6b253626d9452d50e940e
kelly843397 <kelly.soumbo@gmail.com>
2024-10-08 20:00:03 +0200

    update file index.php and create file Router.php

--- services/Router.php
+++ services/Router.php
@@ -2,35 +2,37 @@
 
 class Router
 {
-    private $routes = []; // Tableau qui va stocker les routes ajoutées
-
-    // Fonction pour ajouter une route
-    public function addRoute($path, $controller, $method)
+    public function handleRequest(array $get)
     {
-        // On ajoute une route au tableau avec le chemin, le contrôleur, et la méthode à appeler
-        $this->routes[$path] = ['controller' => $controller, 'method' => $method];
-    }
+        // Instanciation des contrôleurs nécessaires
+        $productController = new ProductController();
+        $cartController = new CartController();
+        $defaultController = new DefaultController();
 
-    // Fonction qui va être appelée pour traiter la requête
-    public function dispatch()
-    {
-        // Récupération de l'URL demandée, sans les slashes au début et à la fin
-        $uri = trim($_SERVER['REQUEST_URI'], '/');
-
-        // On vérifie si l'URL demandée existe dans nos routes
-        if (isset($this->routes[$uri])) {
-            $route = $this->routes[$uri]; // On récupère la route correspondante
-            $controllerName = $route['controller']; // Le contrôleur à appeler
-            $methodName = $route['method']; // La méthode du contrôleur à appeler
-
-            // On instancie le contrôleur
-            $controller = new $controllerName();
-
-            // On appelle la méthode du contrôleur
-            $controller->$methodName();
-        } else {
-            // Si l'URL n'est pas dans les routes, on affiche une erreur 404
-            echo "404 - Page non trouvée";
+        // Gestion des routes
+        if (isset($get['route']) && $get['route'] === 'home') {
+            $defaultController->home();
+        }
+        else if (isset($get['route']) && $get['route'] === 'cart') {
+            $cartController->viewCart();
+        }
+        else if (isset($get['route']) && $get['route'] === 'products') {
+            if (isset($get['category_id'])) {
+                $productController->index($get['category_id']);
+            } else {
+                $productController->index(); // Affiche tous les produits si aucune catégorie n'est spécifiée
+            }
+        }
+        else if (isset($get['route']) && $get['route'] === 'search') {
+            if (isset($get['query'])) {
+                $productController->search($get['query']);
+            } else {
+                echo "Veuillez entrer un mot-clé pour la recherche.";
+            }
+        }
+        else {
+            // Si aucune route n'est spécifiée, afficher la page d'accueil
+            $defaultController->home();
         }
     }
 }
