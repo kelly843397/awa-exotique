c448e38201f43c0c9deeb6b19a368c4d64f8dc76
kelly843397 <kelly.soumbo@gmail.com>
2024-10-11 21:15:36 +0200

    adding routes (orderstatus : findAll, create, update and delete)

--- services/Router.php
+++ services/Router.php
@@ -1,38 +1,13 @@
 <?php
 
-class Router
-{
-    public function handleRequest(array $get)
-    {
-        // Instanciation des contrôleurs nécessaires
-        $productController = new ProductController();
-        $cartController = new CartController();
-        $defaultController = new DefaultController();
+use Pecee\SimpleRouter\SimpleRouter;
 
-        // Gestion des routes
-        if (isset($get['route']) && $get['route'] === 'home') {
-            $defaultController->home();
-        }
-        else if (isset($get['route']) && $get['route'] === 'cart') {
-            $cartController->viewCart();
-        }
-        else if (isset($get['route']) && $get['route'] === 'products') {
-            if (isset($get['category_id'])) {
-                $productController->index($get['category_id']);
-            } else {
-                $productController->index(); // Affiche tous les produits si aucune catégorie n'est spécifiée
-            }
-        }
-        else if (isset($get['route']) && $get['route'] === 'search') {
-            if (isset($get['query'])) {
-                $productController->search($get['query']);
-            } else {
-                echo "Veuillez entrer un mot-clé pour la recherche.";
-            }
-        }
-        else {
-            // Si aucune route n'est spécifiée, afficher la page d'accueil
-            $defaultController->home();
-        }
-    }
-}
+SimpleRouter::get('/orderstatus', [OrderStatusController::class, 'findAll']);
+SimpleRouter::get('/orderstatus/create', [OrderStatusController::class, 'create']);
+SimpleRouter::post('/orderstatus/create', [OrderStatusController::class, 'create']);
+SimpleRouter::get('/orderstatus/update/{id}', [OrderStatusController::class, 'update']);
+SimpleRouter::post('/orderstatus/update/{id}', [OrderStatusController::class, 'update']);
+SimpleRouter::get('/orderstatus/delete/{id}', [OrderStatusController::class, 'delete']);
+
+// Lance le routeur
+SimpleRouter::start();
