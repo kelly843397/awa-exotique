9af2ca81880100b6528b0b2ac6542cd012b5c4f8
kelly843397 <kelly.soumbo@gmail.com>
2024-10-13 23:54:42 +0200

    update SimpleRouter in file

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 
+// Activer l'affichage des erreurs
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 session_start();  // Démarrer la session pour gérer les tokens CSRF
 
 // charge l'autoload de composer (chargement automatique des classes et dépendances)
 require "vendor/autoload.php";
+
+// Importer SimpleRouter
+use Pecee\SimpleRouter\SimpleRouter;
 
 // charge le contenu du .env dans $_ENV
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
@@ -15,3 +22,8 @@ $csrfManager = new CSRFTokenManager();
 // Générer un token CSRF
 $token = $csrfManager->generateCSRFToken();
 
+// Charger les routes
+require_once __DIR__ . '/services/Router.php';
+
+// Lancer le routeur
+SimpleRouter::start();
