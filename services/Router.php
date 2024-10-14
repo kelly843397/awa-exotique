e22404e892ba496f9763f3491534d0819c3ade30
kelly843397 <kelly.soumbo@gmail.com>
2024-10-14 15:27:38 +0200

    update route

--- services/Router.php
+++ services/Router.php
@@ -1,47 +1,21 @@
 <?php
 
+// Import de SimpleRouter
 use Pecee\SimpleRouter\SimpleRouter;
 use App\Controllers\OrderStatusController;
 
-// Route pour la page d'accueil
+// Route pour l'URL racine (page d'accueil)
 SimpleRouter::get('/', function() {
-    // Par exemple, redirige vers la liste des statuts
-    return header('Location: /orderstatus');
+    SimpleRouter::response()->redirect('/orderstatus');
 });
 
-// Définir les routes pour OrderStatusController
-// Route pour afficher tous les statuts de commande
-/*SimpleRouter::get('/orderstatus', [OrderStatusController::class, 'findAll']);
-SimpleRouter::get('/orderstatus', 'App\Controllers\OrderStatusController@findAll');
-
-// Route pour afficher le formulaire de création de statut de commande
+// Utilisation de SimpleRouter pour définir les routes pour OrderstatusController
+SimpleRouter::get('/orderstatus', [OrderStatusController::class, 'findAll']);
 SimpleRouter::get('/orderstatus/create', [OrderStatusController::class, 'create']);
 SimpleRouter::post('/orderstatus/create', [OrderStatusController::class, 'create']);
-
-// Route pour mettre à jour un statut de commande
 SimpleRouter::get('/orderstatus/update/{orderId}', [OrderStatusController::class, 'update']);
 SimpleRouter::post('/orderstatus/update/{orderId}', [OrderStatusController::class, 'update']);
+SimpleRouter::post('/orderstatus/delete/{orderId}', [OrderStatusController::class, 'delete']);
 
-
-
-// Route pour supprimer un statut de commande
-SimpleRouter::get('/orderstatus/delete/{orderId}', [OrderStatusController::class, 'delete']);
-SimpleRouter::post('/orderstatus/delete/{orderId}', [OrderStatusController::class, 'delete']);*/
-
-// Définir les routes pour OrderStatusController
-// Route pour afficher tous les statuts de commande
-SimpleRouter::get('/orderstatus', 'App\Controllers\OrderStatusController@findAll');
-
-// Route pour afficher le formulaire de création de statut de commande
-SimpleRouter::get('/orderstatus/create', 'App\Controllers\OrderStatusController@create');
-SimpleRouter::post('/orderstatus/create', 'App\Controllers\OrderStatusController@create');
-
-// Route pour mettre à jour un statut de commande
-SimpleRouter::get('/orderstatus/update/{orderId}', 'App\Controllers\OrderStatusController@update');
-SimpleRouter::post('/orderstatus/update/{orderId}', 'App\Controllers\OrderStatusController@update');
-
-// Route pour supprimer un statut de commande
-SimpleRouter::post('/orderstatus/delete/{orderId}', 'App\Controllers\OrderStatusController@delete');
-
-
-
+// Lancer le routeur pour traiter la requête
+SimpleRouter::start();
