44ec0206ce9d817a243a26a56a6f7e95f658ffcc
kelly843397 <kelly.soumbo@gmail.com>
2024-10-14 15:33:10 +0200

    create file Order.php

---
+++ models/Order.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+class Order
+{
+    private int $id;
+    private int $userId;
+    private string $orderDate;
+    private string $status;
+
+    // Getter et Setter pour l'ID
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    // Getter et Setter pour user_id
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): void
+    {
+        $this->userId = $userId;
+    }
+
+    // Getter et Setter pour order_date
+    public function getOrderDate(): string
+    {
+        return $this->orderDate;
+    }
+
+    public function setOrderDate(string $orderDate): void
+    {
+        $this->orderDate = $orderDate;
+    }
+
+    // Getter et Setter pour status
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+}
