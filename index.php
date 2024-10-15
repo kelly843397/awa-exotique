7be27ea47a97e0a40cecdf8fd8fa03897ba98f80
kelly843397 <kelly.soumbo@gmail.com>
2024-10-15 21:10:48 +0200

    update file

--- index.php
+++ index.php
@@ -7,13 +7,14 @@ error_reporting(E_ALL);
 // charge l'autoload de composer (chargement automatique des classes et dépendances)
 require "vendor/autoload.php";
 
+// Charger le fichier Router.php
+require __DIR__ . '/services/Router.php';
+
 session_start();  // Démarrer la session pour gérer les tokens CSRF
 
 // charge le contenu du fichier .env dans $_ENV
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
-use App\Services\CSRFTokenManager;
 
 // Instancier le CSRFTokenManager
 $csrfManager = new CSRFTokenManager();
@@ -21,12 +22,7 @@ $csrfManager = new CSRFTokenManager();
 // Générer un token CSRF
 $token = $csrfManager->generateCSRFToken();
 
-
-// Importer SimpleRouter
-use Pecee\SimpleRouter\SimpleRouter;
-
 // Charger les routes
 require_once __DIR__ . '/services/Router.php';
 
-// Lancer le routeur pour traiter la requête
-SimpleRouter::start();
+
